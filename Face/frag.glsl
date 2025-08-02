7c2316903293a7877c681d802eb38e0495d5244f
Rahat Adnan <sthep90@gmail.com>
2025-08-02 13:30:53 +0600

    Update frag.glsl
    1. Detached color from createCircle procedure
    2. used mask to calculate facial objects and multiplied with color vec3
    3. cleanup

--- Face/frag.glsl
+++ Face/frag.glsl
@@ -1,10 +1,7 @@
-
-float negateCoord(float coord){return (1.0 - coord);}
-
-vec3 createCircle(vec2 area,float radius,vec3 color){
+float createCircle(vec2 area,float radius){
     float d = length(area);
     float circle = smoothstep(radius+0.02,radius,d);
-    return circle*color;
+    return circle;
 
 }
 
@@ -15,9 +12,17 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     // Normalized pixel coordinates (from 0 to 1)
     vec2 uv = (fragCoord/iResolution.xy) * 2.0 - 1.0;//center to 0.5 and all corners to 0.1;
     uv.x*=iResolution.x /iResolution.y; // To maintain aspect ratio
-    vec3 color = createCircle(uv,0.5,vec3(1., 1., 1.));
-    color-= createCircle(uv-vec2(-0.2,0.13),0.08,vec3(0.32, 0.32, 0.91));
-    color-= createCircle(uv-vec2(0.2,0.13),0.08,vec3(0.32, 0.32, 0.91));
+    float mask = createCircle(uv,0.5);
+    mask-= createCircle(uv-vec2(-0.2,0.13),0.08); //eye 1
+    mask-= createCircle(uv-vec2(0.2,0.13),0.08); //eye 2
+
+    //mouth
+    float mouth = createCircle(uv-vec2(-0.001,-0.2),0.2);
+    float mouthSub = createCircle(uv-vec2(-0.001,-0.15),0.2);
+    mouth-=mouthSub;
+    mask-=mouth;
+    vec3 color = mask*vec3(1.0, 1.0, 0.0);
+
     // Output to screen
     fragColor = vec4(color,1.0);
 }
